ab1442f05e9b18e6e860d08dde82f3157fc542c5
Svenlaa <sven.lammertink@hotmail.com>
2022-11-03 09:49:06 +0100

    Initial Commit 🎉

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>v2.Svenlaa.com</title>
+        <meta
+          name="description"
+          content="The second generation of Svenlaa.com"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p>v2.Svenlaa.com</p>
+      </main>
+    </>
+  );
+};
+
+export default Home;
